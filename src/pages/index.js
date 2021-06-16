42a5aa0058af3bb8072f1c44ce1900e6a32e2474
brianpompey <brianthescholar@gmail.com>
2021-06-15 22:03:57 -0400

    now time to add my own content

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => (
     <Seo title="Home" />
     <h1>My name is Chef Pompeii</h1>
     <p>Welcome to my new website.</p>
-    <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
